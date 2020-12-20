7fd2a8154ef311123904bd3aba2b2ce091227a2f
TerranCivilian <63529094+TerranCivilian@users.noreply.github.com>
2020-12-20 13:44:31 -0500

    cache simulator almost done

--- cache/csim.c
+++ cache/csim.c
@@ -7,43 +7,52 @@
 #include <errno.h>
 #include <string.h>
 
+int VERBOSE = 0;
+
+typedef struct {
+	int hits;
+	int misses;
+	int evictions;
+} Result;
+
 typedef struct {
 	int s;
 	int E;
 	int b;
-	char *file_name;
+	const char *trace_file_path;
 } Input;
 
 typedef struct {
 	int S;  // number of sets in the cache
 	int s;  // number of set selector bits
 	int E;  // number of cache lines per set
-	int B;  // number of bytes per block
 	int b;  // number of block offset bits
 } Config;
 
 typedef struct {
 	int valid;
 	int tag;
-	int offset;
 } Line;
 
 typedef struct {
 	Line *lines;
 	int E;
+	int *lru_queue;
 } Set;
 
 typedef struct {
 	Set *sets;
+	int s;
 	int S;
+	int b;
 } Cache;
 
-int parse_arg(char *arg)
+int parse_int(char *str)
 {
 	char *end;
 	errno = 0;
-	long i = strtol(arg, &end, 10);
-	if (arg == end)
+	long i = strtol(str, &end, 10);
+	if (str == end)
 		return -1;
 	if (errno != 0 && i == 0)
 		return -1;
@@ -56,22 +65,25 @@ int parse_input(Input *input, int argc, char *argv[])
 {
 	opterr = 0;
 	char opt;
-	while ((opt = getopt(argc, argv, "+s:E:b:t:")) != -1)
+	while ((opt = getopt(argc, argv, "+vs:E:b:t:")) != -1)
 		switch (opt) {
+		case 'v':
+			VERBOSE = 1;
+			break;
 		case 's':
-			if ((input->s = parse_arg(optarg)) < 0)
+			if ((input->s = parse_int(optarg)) < 0)
 				return -1;
 			break;
 		case 'E':
-			if ((input->E = parse_arg(optarg)) < 0)
+			if ((input->E = parse_int(optarg)) < 0)
 				return -1;
 			break;
 		case 'b':
-			if ((input->b = parse_arg(optarg)) < 0)
+			if ((input->b = parse_int(optarg)) < 0)
 				return -1;
 			break;
 		case 't':
-			input->file_name = optarg;
+			input->trace_file_path = optarg;
 			break;
 		default:
 			return -1;
@@ -93,14 +105,23 @@ int build_config(Config *config, Input *input)
 	config->b = input->b;
 	if ((config->S = pow2(input->s)) == -1)
 		return -1;
-	if ((config->B = pow2(input->b)) == -1)
+	return 0;
+}
+
+int init_lru_queue(Set *set, int E)
+{
+	if ((set->lru_queue = (int *) malloc(E * sizeof(int))) == NULL)
 		return -1;
+	for (int i = 0; i < E; ++i)
+		set->lru_queue[i] = i;
 	return 0;
 }
 
 int allocate_cache(Cache *cache, Config *config)
 {
+	cache->s = config->s;
 	cache->S = config->S;
+	cache->b = config->b;
 	// allocate array of sets
 	if ((cache->sets = (Set *) malloc(config->S * sizeof(Set))) == NULL)
 		return -1;
@@ -113,8 +134,107 @@ int allocate_cache(Cache *cache, Config *config)
 		// set valid bits to 0 for each line
 		for (int j = 0; j < config->E; ++j)
 			cache->sets[i].lines[j].valid = 0;
+		if ((init_lru_queue(&cache->sets[i], config->E)) == -1)
+			return -1;
 	}
 
+	return 0;
+}
+
+void rotate_queue(int *lru_queue, int index, int len)
+{
+	int tmp = lru_queue[len-1];
+	lru_queue[len-1] = lru_queue[index];
+	while (index < len-2) {
+		lru_queue[index] = lru_queue[index+1];
+		++index;
+	}
+	lru_queue[len-2] = tmp;
+}
+
+void evict_lru(Set *set, int tag)
+{
+	int lru_index = set->lru_queue[0];
+	set->lines[lru_index].tag = tag;
+	rotate_queue(set->lru_queue, lru_index, set->E);
+}
+
+int update(Set *set, int tag)
+{
+	int added = 0;
+	for (int i = 0; i < set->E; ++i) {
+		if (set->lines[i].valid == 0) {
+			set->lines[i].valid = 1;
+			set->lines[i].tag = tag;
+			added = 1;
+			rotate_queue(set->lru_queue, i, set->E);
+			break;
+		}
+	}
+	if (!added) {
+		evict_lru(set, tag);
+		return 1;
+	}
+	return 0;
+}
+
+void ref_mem(Cache *cache, unsigned long long address, Result *result)
+{
+	// don't need the b bits
+	address >>= cache->b;
+	int index = address & (pow2(cache->s)-1);
+	int tag = address >> cache->s;
+	int hit = 0;
+	for (int i = 0; i < cache->sets[index].E; ++i) {
+		if (cache->sets[index].lines[i].valid &&
+                    cache->sets[index].lines[i].tag == tag) {
+			result->hits++;
+			hit = 1;
+			if (VERBOSE)
+				printf("hit ");
+			break;
+		}
+	}
+	if (!hit) {
+		result->misses++;
+		if (VERBOSE)
+			printf("miss ");
+		int e = update(&cache->sets[index], tag);
+		result->evictions += e;
+		if (VERBOSE && e)
+			printf("eviction ");
+	}
+}
+
+int simulate(Cache *cache, Result *result, const char *trace_file_path)
+{
+	FILE *trace_file;
+	if ((trace_file = fopen(trace_file_path, "r")) == NULL)
+		return -1;
+
+	// parsing the Valgrind memory trace (csapp.cs.cmu.edu/3e/cachelab.pdf page 2)
+	const int STR_SIZE = 32;
+	char line_str[STR_SIZE];
+	while (fgets(line_str, STR_SIZE, trace_file) != NULL) {
+		// can ignore instruction operations
+		if (line_str[0] == 'I')
+			continue;
+		if (VERBOSE) {
+			char *newline = strstr(line_str, "\n");
+			*newline = '\0';
+			printf("%s ", &line_str[1]);
+		}
+		char *end;
+		unsigned long long address = strtoull(&line_str[3], &end, 16);
+		if (line_str[1] == 'M') {
+			ref_mem(cache, address, result);
+			ref_mem(cache, address, result);
+		} else
+			ref_mem(cache, address, result);
+		if (VERBOSE)
+			printf("\n");
+	}
+	fclose(trace_file);
 	return 0;
 }
 
@@ -128,18 +248,26 @@ int main(int argc, char *argv[])
 	}
 
 	// build the Config object with s, E, and b values from user input
-	// then derive S and B values
+	// then derive S value
 	Config config;
 	if ((build_config(&config, &input)) == -1) {
-		fprintf(stderr,
-			"%s: error: input parameters are invalid.\n",
-			argv[0]);
+		fprintf(stderr, "%s: error: input parameters are invalid.\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
+	// Cache = array of Set; Set = array of Line; Line = struct {int,int}
 	Cache cache;
-	allocate_cache(&cache, &config);
+	if (allocate_cache(&cache, &config) == -1) {
+		fprintf(stderr, "%s: error: failed to allocate cache structure.\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-	printSummary(0, 0, 0);
+	Result result = {0, 0, 0};
+	if (simulate(&cache, &result, input.trace_file_path) == -1) {
+		fprintf(stderr, "%s: error: cache simulation failed.\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	printSummary(result.hits, result.misses, result.evictions);
 	return 0;
 }
